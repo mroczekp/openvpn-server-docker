119a289f2bd85b71135a0a87916b03bc4d7b974b
piotr.mroczek <piotr.mroczek@myhome.pl>
2018-02-12 19:47:18 +0100

    eveloution

--- download.sh
+++ download.sh
@@ -1,12 +1,22 @@
-#!/usr/bin/env sh
+!/usr/bin/env sh
 
 wget https://github.com/mroczekp/openvpn-server-docker/archive/master.zip 
 apt-get update
-apt-get install unzip docker.io -y
+apt-get install unzip docker.io cgroup-lite -y
 
 unzip master.zip
 
 cd openvpn-server-docker-master
+
+MY_IP=`curl ifconfig.co`
+sed -i "s/VPN.SERVERNAME.COM/$MY_IP/g" makeConfig.sh
+
+
+
+
+
+
+
 
 
 
