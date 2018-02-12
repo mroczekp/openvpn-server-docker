01905cbd7a03534a4b8e484d4e382342d17a3532
piotr.mroczek <piotr.mroczek@myhome.pl>
2018-02-12 19:30:50 +0100

    evolution

---
+++ openvpn-server-docker-master/download.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+wget https://github.com/mroczekp/openvpn-server-docker/archive/master.zip 
+apt-get update
+apt-get install unzip docker.io -y
+
+unzip master.zip
+
+cd openvpn-server-docker-master
+
+
+
+
+
+
+
+
+
+
+
