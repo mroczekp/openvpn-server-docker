228bc125cd4b6cddf7f2834e79f46a172a902e6c
piotr.mroczek <piotr.mroczek@myhome.pl>
2018-02-12 17:07:58 +0100

    evolution of installer

--- makeConfig.sh
+++ makeConfig.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-OVPN_DATA=`pwd` + '/data'
+OVPN_DATA=`pwd`'/data'
+
 
 docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u tcp://VPN.SERVERNAME.COM:80
